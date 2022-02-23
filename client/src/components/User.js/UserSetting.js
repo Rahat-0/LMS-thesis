7791e5e9c5f34d7b0abb9ff0441748ff6849c3b8
rahat <201853082050@nuist.edu.cn>
2022-02-23 23:20:03 +0600

    client: admin/sutdentEdit component performence optimized

--- client/src/components/User.js/UserSetting.js
+++ client/src/components/User.js/UserSetting.js
@@ -1,145 +1,234 @@
-import React, {useEffect, useState} from 'react'
-import axios from 'axios'
-import cookies from 'js-cookie'
-import jwt from 'jwt-decode'
-import {useNavigate} from 'react-router-dom'
-import {ToastContainer, toast} from 'react-toastify'
-import ComPopUpConfirm from '../common/ComPopUpConfirm'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import cookies from "js-cookie";
+import jwt from "jwt-decode";
+import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import ComPopUpConfirm from "../common/ComPopUpConfirm";
 
 function UserSetting() {
-    const [data, setData] = useState('')
-    const [isSetting, setIsSetting] = useState(false)
-    const [deletes, setDelete] = useState(false);
-    const [deactivate, setDeactivate] = useState(false);
+  const [data, setData] = useState([]);
 
-    const navigate = useNavigate()
-    const key =  cookies.get('auth')
-    const auth = key.split(' ')[1]
-    const { schoolId } = jwt(auth);
-    useEffect(() => {
-      if(key){
-        axios.get(`api/student/${schoolId}`, {headers : {auth : key }})
-        .then((result)=>{
-            console.log(result.data)
-            setData(result.data)
+  const [isSetting, setIsSetting] = useState(false);
+  const [deletes, setDelete] = useState(false);
+  const [deactivate, setDeactivate] = useState(false);
+
+  const navigate = useNavigate();
+  const key = cookies.get("auth");
+  const auth = key.split(" ")[1];
+  const { schoolId } = jwt(auth);
+
+  useEffect(() => {
+    if (key) {
+      axios
+        .get(`api/student/${schoolId}`, { headers: { auth: key } })
+        .then((result) => {
+          console.log(result.data);
+          setData(result.data);
         })
-        .catch(err => console.log(err))
-      }
-      
-    }, [])
-    
-
-
-
-    const deactiveHandler =()=>{
-        axios.put('api/student/status', {schoolId}, {headers : {auth : key}})
-        .then((result)=>{
-            if(result.data.message === 'success'){
-                navigate('/')
-                cookies.set('auth', '')
-            }else{
-                console.log(result.data)
-                toast.error('deactivation failed!')
-            }
-        })
-        .catch(err => console.log(err))
+        .catch((err) => console.log(err));
     }
-    const deleteHandler =()=>{
-        alert('deleted')
-    }
+  }, []);
+
+  const deactiveHandler = () => {
+    axios
+      .put("api/student/status", { schoolId }, { headers: { auth: key } })
+      .then((result) => {
+        if (result.data.message === "success") {
+          navigate("/");
+          cookies.set("auth", "");
+        } else {
+          console.log(result.data);
+          toast.error("deactivation failed!");
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+  const deleteHandler = () => {
+    alert("deleted");
+  };
+
+  const editHandler = (e) => {
+    e.preventDefault();
+    console.log(data);
+  };
   return (
-      <div>
-           <div className="bg-red-400 h-16">sr only</div>
-           <ComPopUpConfirm deleted={deleteHandler} deactived={deactiveHandler} states={[deletes, setDelete, deactivate, setDeactivate]} />
-        <div className='bg-white md:flex md:justify-around'>
-        
-        <div className='hidden md:block md:w-3/12'>
-            <h3 className='font-bold text-4xl py-3'>Settings</h3>
-            <ul>
-                <li className={`${isSetting ? "" : 'bg-red-300 text-white' } p-2 bg-gray-100 my-2 rounded-lg hover:bg-red-400 cursor-pointer`} onClick={()=>setIsSetting(false)}> Public profile</li>
-                <li className={`${isSetting ? 'bg-red-300 text-white' : '' } p-2 bg-gray-100 my-2 rounded-lg hover:bg-red-400 cursor-pointer`}onClick={()=>setIsSetting(true)}>Account settings</li>
-            </ul>
+    <div>
+      <div className="bg-red-400 h-16">sr only</div>
+
+      {/* popup confirmation component */}
+      <ComPopUpConfirm
+        deleted={deleteHandler}
+        deactived={deactiveHandler}
+        states={[deletes, setDelete, deactivate, setDeactivate]}
+      />
+
+      <div className="bg-white md:flex md:justify-around">
+        <div className="hidden md:block md:w-3/12">
+          <h3 className="font-bold text-4xl py-3">Settings</h3>
+          <ul>
+            <li
+              className={`${
+                isSetting ? "" : "bg-red-300 text-white"
+              } p-2 bg-gray-100 my-2 rounded-lg hover:bg-red-400 cursor-pointer`}
+              onClick={() => setIsSetting(false)}
+            >
+              {" "}
+              Public profile
+            </li>
+            <li
+              className={`${
+                isSetting ? "bg-red-300 text-white" : ""
+              } p-2 bg-gray-100 my-2 rounded-lg hover:bg-red-400 cursor-pointer`}
+              onClick={() => setIsSetting(true)}
+            >
+              Account settings
+            </li>
+          </ul>
         </div>
-        {isSetting ? 
-        
-        <div className='md:w-8/12'>
-            <h3 className='text-center md:text-left text-3xl p-2 bg-gray-200 md:bg-white'>Profile Setting</h3>
-            <div className='flex flex-col justify-between  md:items-start m-2 space-y-3 border p-2 rounded shadow-lg'>
-                <div className='flex justify-between w-full p-2 border rounded bg-red-50 shadow'>
-                        <div className='px-1'>
-                            <h4 className='font-bold text-lg text-red-700'>deactivate this account</h4>
-                            <p className='text-sm'>will sign out and no longer signin untill activated from librarians</p>
-                        </div>
-                        <input onClick={()=> setDeactivate(true)} className=' rounded px-2  bg-yellow-700 text-white font-bold hover:bg-white hover:text-black cursor-pointer border-2 border-yellow-500'  type='button' value='deactivate' />
+        {isSetting ? (
+          <div className="md:w-8/12">
+            <h3 className="text-center md:text-left text-3xl p-2 bg-gray-200 md:bg-white">
+              Profile Setting
+            </h3>
+            <div className="flex flex-col justify-between  md:items-start m-2 space-y-3 border p-2 rounded shadow-lg">
+              <div className="flex justify-between w-full p-2 border rounded bg-red-50 shadow">
+                <div className="px-1">
+                  <h4 className="font-bold text-lg text-red-700">
+                    deactivate this account
+                  </h4>
+                  <p className="text-sm">
+                    will sign out and no longer signin untill activated from
+                    librarians
+                  </p>
                 </div>
+                <input
+                  onClick={() => setDeactivate(true)}
+                  className=" rounded px-2  bg-yellow-700 text-white font-bold hover:bg-white hover:text-black cursor-pointer border-2 border-yellow-500"
+                  type="button"
+                  value="deactivate"
+                />
+              </div>
 
-                <div className='flex justify-between w-full p-2 border rounded bg-red-50 shadow'>
-                        <div className='px-1'>
-                            <h4 className='font-bold text-lg text-red-700'>delete this account</h4>
-                            <p className='text-sm'>will delete everything permanenly. </p>
-                        </div>
-                        <input onClick={()=>setDelete(true)} className=' rounded px-2 bg-red-900 text-white font-bold hover:bg-white hover:text-black cursor-pointer border-2 border-red-700'  type='button' value='delete' />
+              <div className="flex justify-between w-full p-2 border rounded bg-red-50 shadow">
+                <div className="px-1">
+                  <h4 className="font-bold text-lg text-red-700">
+                    delete this account
+                  </h4>
+                  <p className="text-sm">will delete everything permanenly. </p>
                 </div>
-
-                
+                <input
+                  onClick={() => setDelete(true)}
+                  className=" rounded px-2 bg-red-900 text-white font-bold hover:bg-white hover:text-black cursor-pointer border-2 border-red-700"
+                  type="button"
+                  value="delete"
+                />
+              </div>
             </div>
-        </div>
-    :
-    
-     <div className='md:w-8/12'>
-            <h3 className='text-center md:text-left text-3xl p-2 bg-gray-200 md:bg-white'>Public profile</h3>
-            <div className='flex flex-col md:flex-row md:w-9/12  justify-center items-center'>
-                <div className='flex justify-center w-56 h-56 m-2'>
-                    <img className=' shadow-2xl bg-cover md:w-60 object-contain text-center border-8 rounded-full' src={`../image/${data.profileImage}`} alt='profile pictures' />
-                </div>
-                <div className='text-white'>
-                    <button className=' md:w-52 block md:ml-10 md:my-6 bg-green-500 w-80 my-2 rounded-lg py-2 hover:bg-green-800'>Change picture</button>
-                    <button className=' md:w-52 block md:ml-10 md:my-6 bg-red-500 w-80 my-2 rounded-lg py-2 hover:bg-red-800'>Edit profile</button>
-                </div>
+          </div>
+        ) : (
+          <form onSubmit={editHandler} className="md:w-8/12">
+            <h3 className="text-center md:text-left text-3xl p-2 bg-gray-200 md:bg-white">
+              Public profile
+            </h3>
+            <div className="flex flex-col md:flex-row md:w-9/12  justify-center items-center">
+              <div className="flex justify-center w-56 h-56 m-2">
+                <img
+                  className=" shadow-2xl bg-cover md:w-60 object-contain text-center border-8 rounded-full"
+                  src={`../image/${data.profileImage}`}
+                  alt="profile pictures"
+                />
+              </div>
+              <div className="text-white">
+                <button className=" md:w-52 block md:ml-10 md:my-6 bg-green-500 w-80 my-2 rounded-lg py-2 hover:bg-green-800">
+                  Change picture
+                </button>
+                <button
+                  type="submit"
+                  className=" md:w-52 block md:ml-10 md:my-6 bg-red-500 w-80 my-2 rounded-lg py-2 hover:bg-red-800"
+                >
+                  Edit profile
+                </button>
+              </div>
             </div>
-            <div className='flex flex-col  justify-center md:items-start items-center'>
-                <div className='flex flex-col md:m-2 md:flex-row md:w-9/12 md:space-x-5 justify-center items-center '>
+            <div className="flex flex-col  justify-center md:items-start items-center">
+              <div className="flex flex-col md:m-2 md:flex-row md:w-9/12 md:space-x-5 justify-center items-center ">
+                <div className="md:w-6/12">
+                  <label className="font-bold">full name</label>
+                  <input
+                    className="block w-80 md:w-full rounded-lg px-2 py-2 bg-green-100 outline-none"
+                    onChange={(e) => setData({ ...data, name: e.target.value })}
+                    value={data.name}
+                    type="text"
+                  />
+                </div>
+                <div className="md:w-6/12">
+                  <label className="font-bold">schoolID</label>
+                  <input
+                    className="block w-80 md:w-full rounded-lg px-2 py-2 bg-green-100 outline-none"
+                    disabled
+                    value={data.schoolId}
+                    type="number"
+                  />
+                </div>
+              </div>
+              <div className="md:w-full md:m-2">
+                <label className="block font-bold">email</label>
+                <input
+                  className="w-80 md:w-9/12 rounded-lg px-2 py-2 bg-green-100 outline-none"
+                  onChange={(e) => setData({ ...data, email: e.target.value })}
+                  value={data.email}
+                  type="text"
+                />
+              </div>
 
-                    <div className='md:w-6/12'>
-                        <label className='font-bold'>full name</label>
-                        <input className='block w-80 md:w-full rounded-lg px-2 py-2 bg-green-100 outline-none' value={data.name} type='text' />
-                    </div>
-                    <div className='md:w-6/12'>
-                        <label className='font-bold'>schoolID</label>
-                        <input className='block w-80 md:w-full rounded-lg px-2 py-2 bg-green-100 outline-none' value={data.schoolId} type='number' />
-                    </div>
-                </div>
-                <div className='md:w-full md:m-2'>
-                        <label className='block font-bold'>email</label>
-                        <input className='w-80 md:w-9/12 rounded-lg px-2 py-2 bg-green-100 outline-none' value={data.email} type='text' />
-                </div>
+              <div className="md:w-full md:m-2">
+                <label className="block font-bold">user type</label>
+                <input
+                  className="w-80 md:w-9/12 rounded-lg px-2 py-2 bg-green-100 outline-none "
+                  disabled
+                  value={data.userType}
+                  type="text"
+                />
+              </div>
 
-                <div className='md:w-full md:m-2'>
-                        <label className='block font-bold'>user type</label>
-                        <input className='w-80 md:w-9/12 rounded-lg px-2 py-2 bg-green-100 outline-none ' value={data.userType}  type='text'   />
-                </div>
+              <div className="md:w-full md:m-2">
+                <label className="block font-bold">mobile</label>
+                <input
+                  className="w-80 md:w-9/12 rounded-lg px-2 py-2 bg-green-100 outline-none"
+                  value="098094832"
+                  type="number"
+                />
+              </div>
 
-                <div className='md:w-full md:m-2'>
-                        <label className='block font-bold'>mobile</label>
-                        <input className='w-80 md:w-9/12 rounded-lg px-2 py-2 bg-green-100 outline-none' value='098094832' type='number' />
-                </div>
+              <div className="md:w-full md:m-2">
+                <label className="block font-bold">gender</label>
+                <select
+                  className="w-80 md:w-9/12 rounded-lg px-2 py-2 bg-green-100 outline-none"
+                  onChange={(e) => setData({ ...data, gender: e.target.value })}
+                  value={data.gender}
+                >
+                  <option>male</option>
+                  <option>female</option>
+                </select>
+              </div>
 
-                <div className='md:w-full md:m-2'>
-                        <label className='block font-bold'>Bio</label>
-                        <textarea className='w-80 md:w-9/12 rounded-lg px-2 py-2 bg-green-100 outline-none' value={data.bio} type='textarea' />
-                </div>
+              <div className="md:w-full md:m-2">
+                <label className="block font-bold">Bio</label>
+                <textarea
+                  className="w-80 md:w-9/12 rounded-lg px-2 py-2 bg-green-100 outline-none"
+                  onChange={(e) => setData({ ...data, bio: e.target.value })}
+                  value={data.bio}
+                  type="textarea"
+                />
+              </div>
             </div>
-            
-        </div> 
-    
-    }
-        
-        
-    </div>
-    <ToastContainer />
+          </form>
+        )}
       </div>
-
-  )
+      <ToastContainer />
+    </div>
+  );
 }
 
-export default UserSetting
+export default UserSetting;
--- client/src/components/admin/StudentEdit.js
+++ client/src/components/admin/StudentEdit.js
@@ -9,55 +9,39 @@ function StudentEdit() {
   const {id} = useParams()
   const [visible] = useContext(Menubar);
   
-
   const [submitting, setSubmitting] = useState(false);
   const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([])
 
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [schoolId, setSchoolId] = useState("");
-  const [gender, setGender] = useState("");
-  const [userType, setUserType] = useState("");
-  const [preimg, setPreimg] = useState("");
-  const [proImage, setProImage] = useState();
   const key = Cookies.get("auth");
+  console.log('rendering count')
 
-  useEffect(() => {
-    axios
-      .post("/api/admin/students", { schoolId : id }, { headers: { auth: key } })
-      .then((result) => {
-        setName(result.data.name);
-        setEmail(result.data.email);
-        setSchoolId(result.data.schoolId);
-        setGender(result.data.gender);
-        setUserType(result.data.userType);
-        setPreimg(result.data.profileImage)
-        setLoading(false);
-      })
-      .catch((err) => console.log(err));
-  }, []);
-
-  const preImage = () =>{
-    console.log("preImage calling")
-    axios
-      .post("/api/admin/students", { schoolId : id }, { headers: { auth: key } })
-      .then((result) => {
-        setPreimg(result.data.profileImage)
-      })
-      .catch((err) => console.log(err));
-  }
   
+  useEffect(() => {
+    showData()
+  }, [])
+  
+  const showData =()=>{
+    axios
+    .post("/api/admin/students", { schoolId : id }, { headers: { auth: key } })
+    .then((result) => {
+      setData(result.data)
+      setLoading(false);
+    })
+    .catch((err) => console.log(err));
+  }
 
   const handleUpdate = async (e) => {
     e.preventDefault();
     setSubmitting(true)
     const formData = new FormData();
-      formData.append('name',name)
-      formData.append('email',email)
-      formData.append('schoolId',schoolId)
-      formData.append('gender',gender)
-      formData.append('pre', preimg)
-      formData.append('profileImage',proImage)
+      formData.append('name',data.name)
+      formData.append('email',data.email)
+      formData.append('schoolId',data.schoolId)
+      formData.append('gender',data.gender)
+      formData.append('pre', data.profileImage)
+      formData.append('profileImage',data.proImage)
+
     await axios
       .put(
         "/api/admin/students",
@@ -65,13 +49,13 @@ function StudentEdit() {
         { headers: { auth: key , enctype: "multipart/form-data"} }
       )
       .then((result) => {
-        console.log(result.data)
         if (result.data.error) {
           toast.error(result.data.error, { position: "bottom-left" });
         } else if (result.data.warn) {
           toast.warning(result.data.warn, { position: "bottom-left" });
         } else if (result.data.message) {
           toast.success(result.data.message, { position: "bottom-left" });
+          showData()
         } else {
           toast.error("someting is wrong!", { position: "bottom-left" });
         }
@@ -83,12 +67,9 @@ function StudentEdit() {
       .catch((err) => {
         console.log("custom error here" + err);
       });
-      preImage()
   };
 
-
   const routes = "Student Edit";
-
   return (
     <div
       className={`flex flex-col pt-20 p-5 ${
@@ -123,6 +104,8 @@ function StudentEdit() {
                 <div className="shadow overflow-hidden sm:rounded-md">
                   <div className="px-2 py-8 bg-white sm:p-6">
                     <div className="grid grid-cols-6 gap-6">
+
+    {/* name components */}
                       <div className="col-span-6 sm:col-span-3">
                         <label
                           htmlFor="name"
@@ -131,18 +114,18 @@ function StudentEdit() {
                           Full Name
                         </label>
                         <input
-                          onChange={(e) => setName(e.target.value)}
+                          onChange={(e) => setData({...data, name : e.target.value})}
                           type="text"
                           name="name"
                           placeholder="Enter your name"
-                          value={name}
+                          value={data.name}
                           id="name"
                           required
                           
                           className="mt-1 outline-none p-1  border-b  block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
                         />
                       </div>
-
+    {/* email components */}
                       <div className="col-span-6 sm:col-span-3">
                         <label
                           htmlFor="email"
@@ -151,10 +134,10 @@ function StudentEdit() {
                           Email
                         </label>
                         <input
-                          onChange={(e) => setEmail(e.target.value)}
+                          onChange={(e) => setData({...data, email : e.target.value})}
                           type="text"
                           name="email"
-                          value={email}
+                          value={data.email}
                           placeholder="Enter your email"
                           id="email"
                           required
@@ -162,6 +145,7 @@ function StudentEdit() {
                           className="mt-1 border-b focus:ring-indigo-500  outline-none p-1  focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
                         />
                       </div>
+    {/* schoolID components*/}
                       <div className="col-span-6 sm:col-span-3">
                         <label
                           htmlFor="school-id"
@@ -172,7 +156,7 @@ function StudentEdit() {
                         <input
                           type="number"
                           name="school-id"
-                          value={schoolId}
+                          value={data.schoolId}
                           placeholder="Enter school ID"
                           id="school-id"
                           disabled
@@ -180,7 +164,7 @@ function StudentEdit() {
                           className="mt-1 border-b focus:ring-indigo-500 outline-none p-1 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
                         />
                       </div>
-
+    {/* userType components */}
                       <div className="col-span-6 sm:col-span-3">
                         <label
                           htmlFor="student"
@@ -191,14 +175,14 @@ function StudentEdit() {
                         <input
                           type="text"
                           name="student"
-                          value={userType}
+                          value={data.userType}
                           placeholder="Student"
                           id="student"
                           disabled
                           className="mt-1 border-b focus:ring-indigo-500 outline-none p-1 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
                         />
                       </div>
-
+    {/* profileImage components */}
                       <div className="col-span-6 sm:col-span-3">
                         <label
                           htmlFor="image"
@@ -207,14 +191,14 @@ function StudentEdit() {
                           profile image
                         </label>
                         <input
-                          onChange={(e)=> setProImage(e.target.files[0])}
+                          onChange={(e)=> setData({...data, proImage : e.target.files[0]})}
                           name = 'profileImage'
                           type="file"
                           id="image"
                           className="mt-1 border-b focus:ring-indigo-500 outline-none p-1 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
                         />
                       </div>
-
+    {/* gender components */}
                       <div className="col-span-6 sm:col-span-3">
                         <label
                           htmlFor="gender"
@@ -222,19 +206,17 @@ function StudentEdit() {
                         >
                           Gender
                         </label>
-                        <input
-                          onChange={(e) => setGender(e.target.value)}
-                          type="text"
-                          value={gender}
-                          placeholder="Enter gender"
-                          id="gender"
-                          required
-                          
+                        <select
+                          onChange={(e) => setData({...data, gender : e.target.value})}
+                          value={data.gender}
                           className="mt-1 border-b focus:ring-indigo-500 outline-none p-1 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                        />
+                        >
+                          <option>male</option>
+                          <option>female</option>
+                        </select>
                       </div> 
                     </div>
-
+    {/* submit button components */}
                     <div className="px-4 py-3 bg-white text-right sm:px-6">
                       <Link
                         to={`/admin/studentview/${id}`}
@@ -267,7 +249,6 @@ function StudentEdit() {
                 </div>
               </form>
             </div>
-
             <div className="hidden sm:block" aria-hidden="true">
               <div className="py-5">
                 <div className=""></div>
